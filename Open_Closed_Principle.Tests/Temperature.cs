7e2a2ee326ecf36a504b632f30240dc2d52eb1ec
Stefan Mehnert <StefanMehnert@gmail.com>
2016-04-26 05:25:56 +0200

    Added Tests, completed violation Example with the AvgTemperatureReader

---
+++ Open_Closed_Principle.Tests/Temperature.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using Open_Closed_Principle.Shared;
+
+namespace Open_Closed_Principle.Tests
+{
+    [TestFixture]
+    public class TemperatureTest
+    {
+        private readonly Temperature _oneDegreeCelsius = new Temperature(1, TemperatureUnit.Celsius);
+        private readonly Temperature _twoDegreeCelsius = new Temperature(2, TemperatureUnit.Celsius);
+        private readonly Temperature _oneDegreeFahrenheit = new Temperature(1, TemperatureUnit.Fahrenheit);
+
+        [Test]
+        public void AddCelsiusReturnsSum()
+        {
+            var sumOf1And1Celsius = _oneDegreeCelsius + _oneDegreeCelsius;
+
+            sumOf1And1Celsius.Should().Be(_twoDegreeCelsius);
+        }
+
+        [Test]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void SumDifferentUnitRaisesException()
+        {
+            var differentUnits = _oneDegreeCelsius + _oneDegreeFahrenheit;
+        }
+    }
+}
