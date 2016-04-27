2d20989b8a89a828dcb981cc15a88947d0d1c84e
Stefan Mehnert <StefanMehnert@gmail.com>
2016-04-27 20:52:49 +0200

    Refacatored OCP Valid AvgTemperatureReader

--- Open_Closed_Principle/Valid/AvgTemperatureReader.cs
+++ Open_Closed_Principle/Valid/AvgTemperatureReader.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Open_Closed_Principle.Valid
 {
@@ -6,19 +7,7 @@ namespace Open_Closed_Principle.Valid
     {
         public double GetAverageTemperature(List<ITemperatureSensor> sensors)
         {
-            double total = 0;
-            int counter = 0;
-
-            sensors.ForEach(sensor =>
-            {
-                total += sensor.CurrentTemperature.Value;
-                counter++;
-            });
-
-            if (counter == 0)
-                return 0;
-
-            return total / counter;
+            return sensors.Count == 0 ? 0 : sensors.Average(s => s.CurrentTemperature.Value);
         }
     }
 }
